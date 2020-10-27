24d018943b12abc40a682806f318832a48538eb8
abbylute <aby.lute@gmail.com>
2020-10-27 14:30:50 -0700

    update

--- plot_coarse_fine_cell_locations.m
+++ plot_coarse_fine_cell_locations.m
@@ -12,9 +12,16 @@ lon = [states.Lon];
 figure(1);clf; fig = gcf; fig.PaperUnits = 'inches';
 fig.PaperPosition = [0 0 12 12];
 ax1 = usamap(latlim, lonlim);
-plotm(pts.latsf,pts.lonsf,'.','MarkerSize',.001);
+plotm(pts.latf,pts.lonf,'.','MarkerSize',.001);
+plotm(lat,lon,'k','linewidth',.7);
+print('/Volumes/WDPassport/DATA/WRF/Downscaled/fine_cell_coverage.png','-dpng','-r500');      
+        
+        
+figure(2);clf; fig = gcf; fig.PaperUnits = 'inches';
+fig.PaperPosition = [0 0 12 12];
+ax1 = usamap(latlim, lonlim);
+plotm(pts.latc,pts.lonc,'.','MarkerSize',.001);
 plotm(lat,lon,'k','linewidth',.7);
 
-print('/Volumes/WDPassport/DATA/WRF/Downscaled/fine_cell_coverage.png','-dpng','-r300');      
-        
+print('/Volumes/WDPassport/DATA/WRF/Downscaled/coarse_cell_coverage.png','-dpng','-r500');      
         
