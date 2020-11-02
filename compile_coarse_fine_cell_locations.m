a64347930bc6073804328c66aa32cedb8d7c4be0
abbylute <aby.lute@gmail.com>
2020-11-02 08:21:50 -0800

    work with chunks not in us

--- compile_coarse_fine_cell_locations.m
+++ compile_coarse_fine_cell_locations.m
@@ -1,17 +1,25 @@
-mdir = 'DATA/WRF/downscaled/WUS/chunks/';
+mdir = '/home/abby/DATA/WRF/downscaled/WUS/chunks/';
+
+
+finechunks = matfile([mdir,'chunk_coordinates_',num2str(outSRf),'m.mat']);
+finechunks = finechunks.chunk_coords;
+nchunk = size(finechunks.st_col,2);
+    
+lonf = ones(1,0);
+latf = ones(1,0);
+lonc = ones(1,0);
+latc = ones(1,0);
 
 for ii = 1:nchunk
-   m = matfile([mdir,'points_to_model_chunk_',num2str(ii),'.mat']);
-   if ii==1
-       lonf = m.outlonf;
-       latf = m.outlatf;
-       lonc = m.outlonc;
-       latc = m.outlatc;
-   else
+    
+   if finechunks.in_us(ii)==1 % if inside the us, skip this chunk
+       m = matfile([mdir,'points_to_model_chunk_',num2str(ii),'.mat']);
+
       lonf = [lonf; m.outlonf];
       latf = [latf; m.outlatf];
       lonc = [lonc; m.outlonc];
       latc = [latc; m.outlatc]; 
+
    end
 end
 
