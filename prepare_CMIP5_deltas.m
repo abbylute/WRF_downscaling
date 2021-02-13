1a91f457bc0a003b3d2122b30903e9f8bbf9a7af
abbylute <aby.lute@gmail.com>
2021-02-12 16:30:15 -0800

    create a percent delta for solar

--- prepare_CMIP5_deltas.m
+++ prepare_CMIP5_deltas.m
@@ -27,11 +27,6 @@ dwind = cmip.deltaWIND * dt; % wind (m/s), subtract
 dtmean = (dtmax+dtmin)./2;
 clear dtmax dtmin
 
-% to perturb downscaled WRF q2, first convert it to huss, then apply the dhuss
-% perturbation, then convert it back to q2. 
-% huss = double(q2./(1+q2));
-% q2 = double(huss./(1-huss));
-
 
 
 %%%%% interpolate deltas to chunk grid %%%%%
@@ -70,6 +65,33 @@ for chu = 1:nchunk
     end
 end
 
+
+% For solar,
+% convert the absolute delta to a percent data in order to apply to hourly
+% WRF data. Do this by first creating monthly historical solar
+% climatologies at chunk locations. Interpolation has to be done
+% differently since WRF is not on a regular meshgrid.
+msw = matfile('/Volumes/WDPassport/DATA/WRF/Monthly_means/SWDOWN_monthly_CTRL.mat');
+msw = msw.sw_monthly; % units = W/m2
+wlonlat = matfile('/Volumes/WDPassport/DATA/WRF/Monthly_means/lon_lat_hgt.mat');
+wlon = double(wlonlat.lon);
+wlat = double(wlonlat.lat);
+msw = permute(msw,[2,1,3]);
+wlonl = reshape(wlon,size(wlon,1)*size(wlon,2),1);
+wlatl = reshape(wlat,size(wlat,1)*size(wlat,2),1);
+
+msw1 = ones(nchunk,12).*NaN;
+for mm = 1:12
+    F = scatteredInterpolant(wlonl, wlatl, reshape(msw(:,:,mm),size(msw,1)*size(msw,2),1));
+    msw1(:,mm) = F(loncen, latcen); 
+end
+
+% calculate percent deltas:
+dsradwrfp = dsradwrf./msw1;
+%figure(1);clf;scatter(loncen,latcen,100,dsradwrfp(:,1),'filled');colorbar();
+%xlabel('lon');ylabel('lat');title('january fractional change in solar (ratio of delta to historical climatology)');
+
+
 save('/Volumes/WDPassport/DATA/WRF/Downscaled/cmip5_preindustrial_median_deltas_for_WRF_chunks.mat',...
-    'dhusswrf','dlradwrf','dpptwrf','dsradwrf','dwindwrf','dtmeanwrf','-v7.3')
+    'dhusswrf','dlradwrf','dpptwrf','dsradwrfp','dwindwrf','dtmeanwrf','-v7.3')
 
