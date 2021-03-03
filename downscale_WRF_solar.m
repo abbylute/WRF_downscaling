2eac5920c6e3554bfb93f6abcbe4678113d6cac1
abbylute <aby.lute@gmail.com>
2021-03-02 17:45:53 -0800

    allow downscaling to be run hourly and then aggregated to the desired temporal resolution

--- downscale_WRF_solar.m
+++ downscale_WRF_solar.m
@@ -128,7 +128,7 @@ function[] = downscale_WRF_solar(ch, tcfilename, wrfhdir, solar_outTR, finaloutT
 
         % aggregate to desired temporal resolution
         datdown = reshape(datdown,size(datdown,1),finaloutTR, size(datdown,2)/finaloutTR);
-        datdown = mean(datdown,2);
+        datdown = sum(datdown,2);
         
         % round to desired precision
         datdown = round(datdown, 5, 'significant'); % 5 sig figs
