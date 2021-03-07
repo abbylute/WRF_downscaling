29eee55fd5e7d7b703d85abaeab2b8a63782d160
abbylute <aby.lute@gmail.com>
2021-03-07 15:00:40 -0800

    update

--- WRF_downscaling_master.m
+++ WRF_downscaling_master.m
@@ -52,9 +52,13 @@ nchunk = size(finechunks.st_col,2);
 clear finechunks
 ch_to_run = find(in_us>0);
 
-%parfor chu = 1:sum(in_us)
- %   ch = ch_to_run(chu);
-  ch=194; %ch=42; %ch=291;%NCASC; %ch=62;%Olympics %ch=45;%GNP
+chdone = readtable('/Volumes/WDPassport/temp/chunks_done.txt');
+
+
+
+parfor chu = 1:sum(in_us)
+    ch = ch_to_run(chu);
+  %ch=42; %ch=291;%NCASC; %ch=62;%Olympics %ch=45;%GNP
     
         % define where to model at what resolution
         [outlonf, outlatf, outlonc, outlatc] = pick_modeling_locations(ch, outDEMf, outDEMc, outSRf, outSRc, elev_dif_thres, solar_dif_thres, outdir);
@@ -80,7 +84,7 @@ ch_to_run = find(in_us>0);
             warning(['chunk ',num2str(ch),' did not have any points to downscale'])
         end
         toc
-%end
+end
 
 
 
